1b45a3a2effe36116f7961fbe14bb964deb56e4e
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2021-10-28 17:10:00 +0200

    Create dataset for analysis

--- results_processors/results_extraction2.py
+++ results_processors/results_extraction2.py
@@ -3,15 +3,18 @@ from __future__ import print_function
 import itertools
 import os 
 
+import pandas as pd
+
 from results_extraction_utils import load_just_one_result, save_just_one_result
 from utils import create_directory
 
 
 def main():
-    input_path = os.path.join('results', 'union_mode')
+    input_path = os.path.join('results', 'silhouette_study')
     output_path = os.path.join('results')
-    for only_best in [True, False]:
-        results = load_just_one_result(input_path, dataset='synthetic_data', metric='sil')
-        save_just_one_result(results, output_path, dataset='synthetic_data', metric='sil')
+    results = pd.DataFrame()
+    for dataset in ['iris', 'wine', 'breast', 'seeds', 'parkinsons', 'synthetic_data']:
+        results = results.append(load_just_one_result(input_path, dataset=dataset, metric='sil'))
+    results.to_csv(os.path.join(output_path, 'grid_search_results.csv'), index=False)
 
 main()
--- results_processors/results_extraction_utils.py
+++ results_processors/results_extraction_utils.py
@@ -60,16 +60,28 @@ def save_results(results, output_path, only_best):
     results.to_csv(os.path.join(output_path, 'union_results' + ('_only_best' if only_best else '') + '.csv'), index=False)
 
 def load_just_one_result(input_path, dataset, metric):
-    results = pd.DataFrame(columns=['iteration', 'pipeline', 'algorithm', 'score', 'ami', 'max_history_score', 'max_history_score_ami'])
+    results = pd.DataFrame()
     file_name =  dataset + '_' + metric.lower() + '_best_pipeline_0.json'
     with open(os.path.join(input_path, file_name)) as json_file:
         data = json.load(json_file)
         history = data['context']['history']
         for elem in history:
             results = results.append(pd.DataFrame({
+                'dataset': [dataset],
                 'iteration': [elem['iteration']], 
-                'pipeline': [elem['pipeline']], 
-                'algorithm': [elem['algorithm']], 
+                #'pipeline': [elem['pipeline']], 
+                #'algorithm': [elem['algorithm']], 
+                'features': ['None' if elem['pipeline']['features'][0] == 'features_NoneType' else elem['pipeline']['features'][0]], 
+                'features__k': ['None' if elem['pipeline']['features'][0] == 'features_NoneType' else elem['pipeline']['features'][1]['features__k']], 
+                'normalize': ['None' if elem['pipeline']['normalize'][0] == 'normalize_NoneType' else elem['pipeline']['normalize'][0]], 
+                'normalize__with_mean': ['None' if (elem['pipeline']['normalize'][0] == 'normalize_NoneType' or elem['pipeline']['normalize'][0] == 'normalize_MinMaxScaler') else elem['pipeline']['normalize'][1]['normalize__with_mean']], 
+                'normalize__with_std': ['None' if (elem['pipeline']['normalize'][0] == 'normalize_NoneType' or elem['pipeline']['normalize'][0] == 'normalize_MinMaxScaler') else elem['pipeline']['normalize'][1]['normalize__with_std']], 
+                'outlier': ['None' if elem['pipeline']['outlier'][0] == 'outlier_NoneType' else elem['pipeline']['outlier'][0]], 
+                'outlier__n_neighbors': ['None' if elem['pipeline']['outlier'][0] == 'outlier_NoneType' else elem['pipeline']['outlier'][1]['outlier__n_neighbors']], 
+                'algorithm': [elem['algorithm'][0]], 
+                'algorithm__max_iter': [elem['algorithm'][1]['max_iter']], 
+                'algorithm__n_clusters': [elem['algorithm'][1]['n_clusters']], 
+                'score_type': [metric], 
                 'score': [elem['score']], 
                 'ami': [elem['ami']],
                 'max_history_score': [elem['max_history_score']], 
