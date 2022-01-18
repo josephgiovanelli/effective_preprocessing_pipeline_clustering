3df9bb98ef23aa137a77a70be82f92dd185a8e32
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2022-01-18 12:55:11 +0100

    Update data_plotter.py

--- data_plotter.py
+++ data_plotter.py
@@ -1,6 +1,7 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.core.numeric import indices
 import pandas as pd
 
 from fsfc.generic import GenericSPEC
@@ -14,136 +15,124 @@ from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler, Po
 from sklearn.cluster import KMeans, MiniBatchKMeans, MeanShift, SpectralClustering, AgglomerativeClustering, DBSCAN, OPTICS, Birch
 from sklearn.mixture import GaussianMixture
 from sklearn import metrics
+from sklearn.manifold import TSNE
 
 from imblearn.pipeline import Pipeline
 from imblearn.base import BaseSampler
+from imblearn.utils import check_sampling_strategy
 
 from sklearn.preprocessing import label_binarize
 from sklearn.utils.multiclass import check_classification_targets
-from imblearn.utils import check_sampling_strategy
+from sklearn.datasets import make_blobs
+
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
+from s_dbw import S_Dbw
 
 from experiment.pipeline.outlier_detectors import MyOutlierDetector
-from sklearn.preprocessing._function_transformer import FunctionTransformer
+from experiment.utils import datasets
 
-def objective(features, scaler, outlier):
-    data = pd.read_csv('datasets/synthetic_data.csv', header=None)
-    X, y = data.iloc[:, :-1],  data.iloc[:, -1]
-    X.to_csv('X.csv', index=False)
-    y.to_csv('y.csv', index=False)
+def rotate(p, origin=(0, 0), degrees=0):
+    angle = np.deg2rad(degrees)
+    R = np.array([[np.cos(angle), -np.sin(angle)],
+                  [np.sin(angle),  np.cos(angle)]])
+    o = np.atleast_2d(origin)
+    p = np.atleast_2d(p)
+    return np.squeeze((R @ (p.T-o.T) + o.T).T)
 
-    numerical_features = [i for i in range(6)]
-    categorical_features = []
-    if features:
-        myFeatureEngineeringTransformer = GenericSPEC(k=1)
-        myFeatureEngineeringTransformer.fit(X)
-        indeces = myFeatureEngineeringTransformer.get_support()
-        print(indeces)
-        new_i = 0
-        new_numerical_features, new_categorical_features = [], []
-        for i in range(len(indeces)):
-            if indeces[i] == True:
-                if i in numerical_features:
-                    new_numerical_features += [new_i]
-                else: #if i in self.current_categorical_features:
-                    new_categorical_features += [new_i]
-                new_i += 1
-        numerical_features = new_numerical_features
-        categorical_features = new_categorical_features
-
-    myScaler = ColumnTransformer(
-                        transformers=[
-                            ('num', Pipeline(steps=[('normalizing', StandardScaler())]),
-                             numerical_features),
-                            ('cat', Pipeline(steps=[('identity_categorical', FunctionTransformer())]),
-                             categorical_features)])
-
-    myOutlierDetector = MyOutlierDetector(n_neighbors=32)
-
-    myEstimator = KMeans(max_iter=10, n_clusters=3)
-    #myEstimator = KMeans(n_clusters=6, init='k-means++', random_state=0, algorithm='full')
-    #myEstimator = KMedoids(n_clusters=n_clusters, method='pam', init='k-medoids++')
-    #myEstimator = DBSCAN()
-
+def plot(dataset, features, n_features, scaler, outlier, n_clusters, natural_clusters, internal_metric):
+    
+    X, y, original_features = datasets.get_dataset(dataset)
+    """
+    X, y = make_blobs(n_samples=(400, 1000, 300, 300), n_features=2, centers=[(-15, -12.5), (-2.5, -12.5), (-7.5, -22), (0, -22.)], cluster_std=[2, 2, 1.2, 1.2],  shuffle=True, random_state=42)
+    X[:, 1] *= 5
+    X = np.column_stack((X, np.random.uniform(low=5, high=55, size=(2000, 3))))
+    """
+    myFeatureEngineeringTransformer = GenericSPEC(k=n_features)
+    myScaler = StandardScaler()
+    myOutlierDetector = MyOutlierDetector(n_neighbors=100)
+    myEstimator = KMeans(max_iter=10, n_clusters=n_clusters, random_state=42)
     pipe = Pipeline([ 
-        ('features', myFeatureEngineeringTransformer if features else  FunctionTransformer()),
-        ('scaler', myScaler if scaler else  FunctionTransformer()), 
-        ('outlier', myOutlierDetector if outlier else  FunctionTransformer()), 
+        ('features', myFeatureEngineeringTransformer if features else FunctionTransformer()),
+        ('scaler', myScaler if scaler else FunctionTransformer()), 
+        ('outlier', myOutlierDetector if outlier else FunctionTransformer()), 
         ('estimator', myEstimator)
         ])
     
-    y_pred = pipe.fit_predict(X.copy(), y.copy())
-    pd.DataFrame(y_pred).to_csv('y_pred.csv', index=False)
-    if not(features) and not(scaler) and not(outlier):
-        X, y = pd.DataFrame(X), pd.DataFrame(y)
+    y_pred = pipe.fit_predict(X, y)
+    if outlier:
+        Xt, yt = pipe[:-1].fit_resample(X, y)
     else:
-        if outlier:
-            X, y = pipe[:-1].fit_resample(X, y)
+        Xt = pipe[:-1].fit_transform(X)
+        yt = y
+    
+    if internal_metric == 'sil':
+        print(f'internal metric: {silhouette_score(Xt, y_pred)}')
+    elif internal_metric == 'ch':
+        print(f'internal metric: {calinski_harabasz_score(Xt, y_pred)}')
+    elif internal_metric == 'dbi':
+        print(f'internal metric: {-1 * davies_bouldin_score(Xt, y_pred)}')
+    elif internal_metric == 'sdbw':
+        print(f'internal metric: {-1 * S_Dbw(Xt, y_pred)}')
+    print(f'external metric: {metrics.adjusted_mutual_info_score(yt, y_pred)}')
+
+    #Xt = Xt[:, :2]
+    if Xt.shape[1] > 2:
+        Xt = TSNE(n_components=2, random_state=42).fit_transform(Xt)
+        labels = ['TSNE_0', 'TSNE_1', 'TSNE_2']
+    else:
+        if features:
+            indeces = myFeatureEngineeringTransformer.get_support()
+            indeces = [i for i, e in enumerate(indeces) if e == True]
         else:
-            X = pipe[:-1].fit_transform(X)
-        X, y = pd.DataFrame(X), pd.DataFrame(y)
-    print(X, y)
-    #print(pd.DataFrame(y_pred))
+            indeces = range(len(original_features))
+        labels = [original_features[i] for i in indeces]
 
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
     colors = np.array(['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink', 'grey', 'olive', 'cyan'])
     markers = ['o', 'v', '1', 's', 'p', 'P', '*', 'X', 'D', '_']
-    #ax.scatter(X.iloc[:, 0], X.iloc[:, 1],  X.iloc[:, 2], c=[colors[int(i)] for i in y.to_numpy()])
-    ax.scatter(X.iloc[:, 0], 0,  0, c=[colors[int(i)] for i in y_pred])
 
-    #centers = pd.DataFrame(myEstimator.cluster_centers_)
-    min, max = X.min().min(), X.max().max()
-    ax.set_xlim([min, max])
-    ax.set_ylim([min, max])
-    ax.set_zlim([min, max])
-    #ax.scatter(centers.iloc[:, 0], centers.iloc[:, 1], centers.iloc[:, 2], c='cyan', marker='*', s=100)
+    y_to_plot = yt if natural_clusters else y_pred
+    min, max = Xt.min(), Xt.max()
+    centers = myEstimator.cluster_centers_
+    if Xt.shape[1] < 3:
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(Xt[:, 0], Xt[:, 1] if Xt.shape[1] > 1 else [(max+min)/2] * Xt.shape[0], c=[colors[int(i)] for i in y_to_plot])
+        ax.scatter(centers[:, 0], centers[:, 1] if Xt.shape[1] > 1 else [(max+min)/2] * Xt.shape[0], c='cyan', marker='*', s=10)
+    else:
+        ax = fig.add_subplot(1, 1, 1, projection='3d')
+        ax.scatter(Xt[:, 0], Xt[:, 1], Xt[:, 2], c=[colors[int(i)] for i in y_to_plot])
+        ax.scatter(centers[:, 0], centers[:, 1], centers[:, 2], c='cyan', marker='*', s=10)
 
-    
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
 
-    file_name = 'KMeans_k_2'
-    #fig.savefig('plots/' + file_name + '.png')
-    #plt.close('all')
-    print(metrics.silhouette_score(X, y_pred))
+    #ax.set_xlim([min, max])
+    #ax.set_ylim([min, max])
+    ax.set_xlabel(labels[0])
+    ax.set_ylabel(labels[1])
+    if Xt.shape[1] == 3:
+        #ax.set_zlim([min, max])
+        ax.set_zlabel(labels[2])
+    features_str = '__features_' + str(n_features)
+    scaler_str = '__scaler_' + str(scaler)
+    outlier_str = '__outlier_' + str(outlier)
+    clustering_str = '__n_clusters_' + str(n_clusters) + '_natural_' + str(natural_clusters)
+    fig.savefig(dataset + features_str + scaler_str + outlier_str + clustering_str)
     plt.show()
-    return X, metrics.silhouette_samples(X, y_pred)
-    #return metrics.accuracy_score(y, y_pred), metrics.adjusted_mutual_info_score(y.iloc[:, 0].to_numpy(), y_pred), metrics.silhouette_score(X, y_pred), metrics.calinski_harabasz_score(X, y_pred), -1 * metrics.davies_bouldin_score(X, y_pred), file_name
 
-results = pd.DataFrame()
-i = 0
-for features in [True]:
-    for scaler in [True]:
-        for outlier in [False]:
-            """
-            accuracy, ami, silhouette, calinski, davies_bouldin, file_name = objective(features, scaler, outlier)
-            results = results.append({
-                "features": features, 
-                "scaler": scaler, 
-                "outlier": outlier,
-                "file_name": file_name, 
-                "accuracy": accuracy,
-                'ami': ami,
-                'silhouette': silhouette,
-                'calinski': calinski,
-                'davies_bouldin': davies_bouldin
-            }, ignore_index=True)
-            i += 1
-            print("ami=%.2f, slihouette=%.2f, calinski=%.2f, davies_bouldin= %.2f, accuracy=%.2f" % (ami, silhouette, calinski, davies_bouldin, accuracy))
-            results.to_csv('plots/automl_results_2.csv', index=False)
-            """
-            X, silhouette_samples = objective(features, scaler, outlier)
-            X, silhouette_samples = pd.DataFrame(X), pd.DataFrame(silhouette_samples)
-            X.to_csv('Xt.csv', index=False)
-            silhouette_samples.to_csv('silhouette_samples.csv', index=False)
+plot(dataset='synthetic', features=True, n_features=2, scaler=True, outlier=True, n_clusters=4, natural_clusters=True, internal_metric='sil')
+plot(dataset='synthetic', features=True, n_features=2, scaler=True, outlier=True, n_clusters=4, natural_clusters=False, internal_metric='sil')
 
-#results = results.sort_values(by=['silhouette', 'ami', 'accuracy'], ascending=False)
-#results.to_csv('plots/fixed_pipeline_results.csv', index=False)
+plot(dataset='synthetic', features=True, n_features=2, scaler=True, outlier=False, n_clusters=4, natural_clusters=True, internal_metric='sil')
+plot(dataset='synthetic', features=True, n_features=2, scaler=True, outlier=False, n_clusters=4, natural_clusters=False, internal_metric='sil')
+
+plot(dataset='synthetic', features=True, n_features=2, scaler=False, outlier=False, n_clusters=4, natural_clusters=True, internal_metric='sil')
+plot(dataset='synthetic', features=True, n_features=2, scaler=False, outlier=False, n_clusters=4, natural_clusters=False, internal_metric='sil')
+
+plot(dataset='synthetic', features=False, n_features=6, scaler=False, outlier=False, n_clusters=4, natural_clusters=True, internal_metric='sil')
+plot(dataset='synthetic', features=False, n_features=6, scaler=False, outlier=False, n_clusters=4, natural_clusters=False, internal_metric='sil')
 
 '''
 plot silhouette chart
-if config['metric'] == 'SIL':
+if internal_metric == 'SIL':
             fig, ax = plt.subplots(1, 3)
             fig.set_size_inches(18, 7)
             n_clusters = y_pred.iloc[:, 0].unique().size
