ec97602b42d65a587afe9a8a25d0086a9edc4d46
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-01-30 14:31:57 +0100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,5 @@
 docker stop auto_clues
 docker rm auto_clues
 docker build -t auto_clues .
-docker run --name auto_clues --volume $(pwd):/home --detach -t auto_clues
+docker run --name auto_clues --volume $(pwd):/home/effective_preprocessing_pipelines_clustering --detach -t auto_clues
 docker exec auto_clues bash ./scripts/wrapper_experiments.sh
