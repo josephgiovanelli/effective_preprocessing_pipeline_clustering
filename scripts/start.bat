12b366a9b7be90773f86f5358adc3dde50560725
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-02-10 16:44:03 +0100

    Update start.bat

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,5 +1,5 @@
 docker stop auto_clues
 docker rm auto_clues
 docker build -t auto_clues .
-docker run --name auto_clues --volume ${PWD}:/home/effective_preprocessing_pipelines_clustering --detach -t auto_clues
+docker run --name auto_clues --volume %cd%:/home/effective_preprocessing_pipelines_clustering --detach -t auto_clues
 docker exec auto_clues bash ./scripts/wrapper_experiments.sh
