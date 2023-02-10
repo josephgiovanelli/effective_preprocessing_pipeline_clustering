a06385c6a599c7105b55f70791f88c1229679829
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-02-10 16:03:05 +0100

    Another Refactor

--- scripts/wrapper_experiments.sh
+++ scripts/wrapper_experiments.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# printf '\n\nOPTIMIZATION\n\n'
-# python optimization_launcher.py
+printf '\n\nOPTIMIZATION\n\n'
+python experiment/optimization_launcher.py
 
 printf '\n\nSUMMARIZATION\n\n'
-python results_processors/optimization_results_summarizer.py
+python experiment/results_processors/optimization_results_summarizer.py
 
 printf '\n\nDIVERSIFICATION\n\n'
-python results_processors/diversificator.py --experiment exp2 --cadence $1 --max_time 7200
+python experiment/results_processors/diversificator.py --experiment exp1
