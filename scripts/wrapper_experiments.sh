f0724d3f4a6e23bb2c61a2db42779a84fd6d7578
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-07-24 11:01:35 +0200

    Debugging

--- scripts/wrapper_experiments.sh
+++ scripts/wrapper_experiments.sh
@@ -4,11 +4,11 @@
 cd /home/autoclues
 chmod 777 ./scripts/*
 
-printf '\n\nOPTIMIZATION\n\n'
-python experiment/optimization_launcher.py
+# printf '\n\nOPTIMIZATION\n\n'
+# python experiment/optimization_launcher.py
 
-printf '\n\nSUMMARIZATION\n\n'
-python experiment/results_processors/optimization_results_summarizer.py
+# printf '\n\nSUMMARIZATION\n\n'
+# python experiment/results_processors/optimization_results_summarizer.py
 
 printf '\n\nDIVERSIFICATION\n\n'
 python experiment/results_processors/diversificator.py --experiment exp2 --cadence 900 --max_time 7200
