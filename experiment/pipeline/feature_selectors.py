f7750f8dd95a616ed4137f90717f510bad737b15
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-08-02 18:00:47 +0200

    Update feature_selectors.py

--- experiment/pipeline/feature_selectors.py
+++ experiment/pipeline/feature_selectors.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 
 from fsfc.base import BaseFeatureSelector
-
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 class PearsonThreshold(BaseFeatureSelector):
     """Feature selector that removes all correlated features.
@@ -28,6 +29,7 @@ class PearsonThreshold(BaseFeatureSelector):
         self.threshold = threshold
         self.features = None
         self.original_features = None
+        self.to_drops = None
 
     def fit(self, X, y=None):
         """Compability for supervised techniques.
@@ -36,14 +38,54 @@ class PearsonThreshold(BaseFeatureSelector):
         corr = abs(df.corr())
         self.features = range(X.shape[-1])
         self.original_features = range(X.shape[-1])
+        self.to_drops = []
         for i in range(X.shape[-1]):
             if i in self.features:
-                to_drop = [idx for idx, elem in enumerate(corr.iloc[:, i] > self.threshold) if elem and idx != i]
-                self.features = [elem for elem in self.features if elem not in to_drop]
-        return self
+                to_drop = [idx for idx, elem in enumerate(corr.iloc[:, i] > threshold) if elem and idx != i]
+                self.to_drops.append((i, to_drop))
+        feature_sets = self.find_maximum_feature_sets(self.to_drops)
+        # print(f"CANDIDATES:\t{features}")
+        results = {}
+        for feature_set in feature_sets:
+            # print(f"\tcandidate\t{feature_set}")
+            Xt = X[:, feature_set]
+            results[str(feature_set)] = -1
+            for k in range(2, 13):
+                current_sil = silhouette_score(Xt, KMeans(n_clusters=k, random_state=0, n_init="auto").fit_predict(Xt))
+                results[str(feature_set)] = max(results[str(feature_set)], current_sil)
+                # print(f"\t\tk={k}, silhouette:{current_sil}")
+        self.features = np.fromstring(max(results, key=results.get)[1:-1], dtype=int, sep=',')
 
     def _get_support_mask(self):
         return [elem in self.features for elem in self.original_features]
 
     def transform(self, X, y=None):
         return X[:, self.features]
+
+    def is_valid_set(feature_set, z):
+        for x, y_list in z:
+            if x in feature_set:
+                for y in y_list:
+                    if y in feature_set:
+                        return False
+        return True
+
+    def backtrack(z, current_set, index, result):
+        if index == len(z):
+            result.append(current_set[:])
+            return
+
+        for i in range(index, len(z)):
+            x, _ = z[i]
+            if x not in current_set and is_valid_set(current_set + [x], z):
+                current_set.append(x)
+                backtrack(z, current_set, i + 1, result)
+                current_set.pop()
+
+    def find_maximum_feature_sets(z):
+        result = []
+        backtrack(z, [], 0, result)
+
+        max_length = max(len(feature_set) for feature_set in result)
+        max_feature_sets = [feature_set for feature_set in result if len(feature_set) == max_length]
+        return max_feature_sets
