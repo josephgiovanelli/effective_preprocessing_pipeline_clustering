edec70eb69d29fc7cbb267d8dc3a3cde1515d4e4
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-10-24 17:53:12 +0200

    Update performance_summarizer.py

--- experiment/results_processors/performance_summarizer.py
+++ experiment/results_processors/performance_summarizer.py
@@ -19,21 +19,41 @@ def main():
     approaches = ["clustering"]
     get_metric = lambda approach: "ami" if approach == "clustering" else "jaccard"
     params = [1800, 3600, 5400, 7200]
-    datasets = [f"syn{i}" for i in range(20)]
+    datasets = [
+            "avila",
+            "diabetes",
+            "isolet",
+            "postures",
+            "blood",
+            "ecoli",
+            "parkinsons",
+            "seeds",
+            "breast",
+            "iris",
+            "pendigits",
+            "statlog",
+            "synthetic",
+            "wine",
+            "thyroid",
+            "vehicle",
+        ] + [ f"syn{idx}" for idx in range(20)]
     opt_metrics = [
         "optimization_internal_metric_value",
         "optimization_external_metric_value",
     ]
 
-    results = {}
+    results = pd.DataFrame()
     for approach in approaches:
         for param in params:
-            results[f"{approach}_{param}"] = pd.DataFrame()
             for dataset in datasets:
                 try:
-                    results[f"{approach}_{param}"] = pd.concat(
-                        [
-                            results[f"{approach}_{param}"],
+                    results = pd.concat([
+                        results,
+                        pd.concat([
+                            pd.DataFrame({
+                                "approach": [approach]*3,
+                                "time": [param]*3,
+                            }),
                             pd.read_csv(
                                 os.path.join(
                                     input_path,
@@ -41,39 +61,53 @@ def main():
                                     "3",
                                     f"{approach}_0-5_{get_metric(approach)}_{param}.csv",
                                 )
-                            ),
-                        ]
-                    )
+                            )
+                        ], axis=1)
+                    ])
                 except Exception as e:
                     print(e)
 
-            output_path = make_dir(
-                os.path.join(input_path, "summary", f"{approach}_{param}")
-            )
-            results[f"{approach}_{param}"]["optimization_internal_metric_value"] *= -1
-            for metric in opt_metrics:
-                results[f"{approach}_{param}"][metric] = round(
-                    1 - results[f"{approach}_{param}"][metric], 2
-                )
+    output_path = make_dir(
+        os.path.join("/", "home", "results", "diversification", "summary")
+    )
+    results["optimization_internal_metric_value"] *= -1
+    for metric in opt_metrics:
+        results[metric] = round(
+            1 - results[metric], 2
+        )
+    results = results.reset_index()
+    print(results)
+    results.to_csv(os.path.join(output_path, "raw.csv"))
+    grouped = results.groupby(by=["dataset", "approach", "time"]).max()
+    grouped.to_csv(os.path.join(output_path, "max_raw.csv"))
+    grouped[opt_metrics].to_csv(os.path.join(output_path, "max.csv"))
 
-            results[f"{approach}_{param}"].to_csv(os.path.join(output_path, "raw.csv"))
-            grouped = results[f"{approach}_{param}"].groupby(by=["dataset"])
-            for key, agg in {"max": grouped.max(), "avg": grouped.mean()}.items():
-                support = (
-                    agg.index.to_series()
-                    .str.rsplit("n")
-                    .str[-1]
-                    .astype(int)
-                    .sort_values()
-                )
-                agg = agg.reindex(index=support.index)
-                agg = agg[
-                    [
-                        "optimization_internal_metric_value",
-                        "optimization_external_metric_value",
-                    ]
-                ]
-                agg.to_csv(os.path.join(output_path, f"{key}.csv"))
+    for time in params:
+        new_grouped = results[results["time"] == time].groupby(by=["dataset", "approach", "time"]).max().reset_index()
+        new_grouped.to_csv(os.path.join(output_path, f"max_{time}_raw.csv"))
+        new_grouped[["dataset"] + opt_metrics].to_csv(os.path.join(output_path, f"max_{time}.csv"))
+
+    grouped_filtered = results.groupby(by=["dataset"]).max()
+    grouped_filtered.to_csv(os.path.join(output_path, "max_final_raw.csv"))
+    grouped_filtered[opt_metrics].to_csv(os.path.join(output_path, "max_final.csv"))
+
+
+    # for key, agg in {"max": grouped.max(), "avg": grouped.mean()}.items():
+    #     support = (
+    #         agg.index.to_series()
+    #         .str.rsplit("n")
+    #         .str[-1]
+    #         .astype(int)
+    #         .sort_values()
+    #     )
+    #     agg = agg.reindex(index=support.index)
+    #     agg = agg[
+    #         [
+    #             "optimization_internal_metric_value",
+    #             "optimization_external_metric_value",
+    #         ]
+    #     ]
+    #     agg.to_csv(os.path.join(output_path, f"{key}.csv"))
 
 
 if __name__ == "__main__":
