0b25b1fa7cb921a4d9ad00d1a17b1722cca58fde
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-10-27 15:14:46 +0200

    Update performance_summarizer.py

--- experiment/results_processors/performance_summarizer.py
+++ experiment/results_processors/performance_summarizer.py
@@ -12,25 +12,25 @@ def make_dir(path):
     return os.path.normpath(path)
 
 
-def main():
+def get_max_performance(input_path, output_path):
 
-    input_path = os.path.join("/", "home", "results", "diversification", "smbo", "mmr")
+    
 
     approaches = ["clustering"]
     get_metric = lambda approach: "ami" if approach == "clustering" else "jaccard"
     params = [60, 120, 300, 600, 900, 1800, 2700, 3600, 5400, 7200]
     datasets = [
-            "diabetes",
-            "blood",
-            "ecoli",
-            "parkinsons",
-            "seeds",
-            "breast",
-            "iris",
-            "pendigits",
-            "wine",
-            "thyroid",
-            "vehicle",
+            # "diabetes",
+            # "blood",
+            # "ecoli",
+            # "parkinsons",
+            # "seeds",
+            # "breast",
+            # "iris",
+            # "pendigits",
+            # "wine",
+            # "thyroid",
+            # "vehicle",
         ] + [ f"syn{idx}" for idx in range(20)]
     opt_metrics = [
         "optimization_internal_metric_value",
@@ -62,16 +62,13 @@ def main():
                 except Exception as e:
                     print(e)
 
-    output_path = make_dir(
-        os.path.join("/", "home", "results", "diversification", "summary")
-    )
+    
     results["optimization_internal_metric_value"] *= -1
     for metric in opt_metrics:
         results[metric] = round(
             1 - results[metric], 2
         )
     results = results.reset_index()
-    print(results)
     results.to_csv(os.path.join(output_path, "raw.csv"))
     grouped = results.groupby(by=["dataset", "approach", "time"]).max()
     grouped.to_csv(os.path.join(output_path, "max_raw.csv"))
@@ -87,22 +84,43 @@ def main():
     grouped_filtered[opt_metrics].to_csv(os.path.join(output_path, "max_final.csv"))
 
 
-    # for key, agg in {"max": grouped.max(), "avg": grouped.mean()}.items():
-    #     support = (
-    #         agg.index.to_series()
-    #         .str.rsplit("n")
-    #         .str[-1]
-    #         .astype(int)
-    #         .sort_values()
-    #     )
-    #     agg = agg.reindex(index=support.index)
-    #     agg = agg[
-    #         [
-    #             "optimization_internal_metric_value",
-    #             "optimization_external_metric_value",
-    #         ]
-    #     ]
-    #     agg.to_csv(os.path.join(output_path, f"{key}.csv"))
+
+def merge_scores(input_path, output_path):
+
+    measures = {
+        "score": "Norm. score",
+        "optimization_internal_metric_value": "SIL",
+        "optimization_external_metric_value": "AMI",
+        "timing": "Div. time"
+        }
+
+    timing = pd.read_csv(os.path.join("results", "diversification", "timing_clustering.csv"))
+    performance = pd.read_csv(os.path.join("results","diversification","summary","max_raw.csv")).rename(columns={"time": "cadence"})
+    scores = pd.read_csv(os.path.join("results","new_scores.csv"))
+    df = scores.merge(performance, on=["dataset", "cadence"]).merge(timing[["dataset", "cadence", "timing"]], on=["dataset", "cadence"])
+    df = df[["dataset", "cadence"] + list(measures.keys())]
+    df = df.rename(measures, axis="columns")
+    df = df.sort_values(by=["dataset", "cadence"])
+    df = df.groupby("dataset").max()
+    df[measures["timing"]] = df[measures["timing"]].apply(lambda val: '%.2E' % val)
+    df = df.drop("cadence", axis="columns")
+    df.to_csv(os.path.join(output_path, "table2.csv"))
+
+    # for measure in measures.values():
+    #     for idx in range(20):
+    #         df.loc[df["dataset"] == f"syn{idx}", measure] = df.loc[df["dataset"] == f"syn{idx}", measure].cummax()
+
+
+
+def main():
+
+    input_path = os.path.join("/", "home", "results", "diversification", "smbo", "mmr")
+    output_path = make_dir(
+        os.path.join("/", "home", "results", "diversification", "summary")
+    )
+    # get_max_performance(input_path, output_path)
+    merge_scores(input_path, output_path)
+
 
 
 if __name__ == "__main__":
